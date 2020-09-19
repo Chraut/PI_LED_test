848ab4584c7c7a2f951bd12fa5a054469e77d567
chraut <chraut@chraut.chraut>
2020-09-19 13:00:10 +0200

    Commit 2

--- TestPiLED/qtMainWindow.py
+++ TestPiLED/qtMainWindow.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'guiTest.ui'
+# Form implementation generated from reading ui file 'qtMainWindow.ui'
 #
 # Created by: PyQt5 UI code generator 5.11.3
 #
@@ -8,35 +8,59 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
+
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(640, 480)
+        MainWindow.setContextMenuPolicy(QtCore.Qt.NoContextMenu)
+        MainWindow.setInputMethodHints(QtCore.Qt.ImhNone)
+        MainWindow.setToolButtonStyle(QtCore.Qt.ToolButtonIconOnly)
+        MainWindow.setDockOptions(QtWidgets.QMainWindow.AllowTabbedDocks|QtWidgets.QMainWindow.AnimatedDocks)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.butMode1 = QtWidgets.QPushButton(self.centralwidget)
-        self.butMode1.setGeometry(QtCore.QRect(20, 20, 291, 41))
-        self.butMode1.setObjectName("butMode1")
-        self.butMode2 = QtWidgets.QPushButton(self.centralwidget)
-        self.butMode2.setGeometry(QtCore.QRect(330, 20, 291, 41))
-        self.butMode2.setObjectName("butMode2")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
         self.butStopp = QtWidgets.QPushButton(self.centralwidget)
-        self.butStopp.setGeometry(QtCore.QRect(20, 80, 601, 51))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.butStopp.sizePolicy().hasHeightForWidth())
+        self.butStopp.setSizePolicy(sizePolicy)
         self.butStopp.setObjectName("butStopp")
+        self.gridLayout.addWidget(self.butStopp, 1, 0, 1, 2)
+        self.butMode1 = QtWidgets.QPushButton(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.butMode1.sizePolicy().hasHeightForWidth())
+        self.butMode1.setSizePolicy(sizePolicy)
+        self.butMode1.setObjectName("butMode1")
+        self.gridLayout.addWidget(self.butMode1, 0, 0, 1, 1)
+        self.butMode2 = QtWidgets.QPushButton(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.butMode2.sizePolicy().hasHeightForWidth())
+        self.butMode2.setSizePolicy(sizePolicy)
+        self.butMode2.setObjectName("butMode2")
+        self.gridLayout.addWidget(self.butMode2, 0, 1, 1, 1)
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.gridLayout.addItem(spacerItem, 2, 0, 1, 2)
+        self.gridLayout.setRowStretch(0, 1)
+        self.gridLayout.setRowStretch(1, 1)
+        self.gridLayout.setRowStretch(2, 6)
         MainWindow.setCentralWidget(self.centralwidget)
-        self.statusbar = QtWidgets.QStatusBar(MainWindow)
-        self.statusbar.setObjectName("statusbar")
-        MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "GUI LED test"))
+        self.butStopp.setText(_translate("MainWindow", "Stopp"))
         self.butMode1.setText(_translate("MainWindow", "Mode 1"))
         self.butMode2.setText(_translate("MainWindow", "Mode 2"))
-        self.butStopp.setText(_translate("MainWindow", "Stopp"))
 
 
 if __name__ == "__main__":
--- TestPiLED/visMainWindow.py
+++ TestPiLED/visMainWindow.py
@@ -4,15 +4,19 @@ Created on Sep 18, 2020
 @author: chraut
 '''
 from PyQt5.Qt import QMainWindow, QTimer
+from PyQt5.QtCore import Qt
 
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 
 class MainWindow (QMainWindow):
     
     def __init__(self, parent = None):
         super(MainWindow, self).__init__(parent)
         
-    def initUI(self,mainProg,ui):
+        # maximize
+        self.showMaximized()
+        
+    def initUI(self,mainProg,ui):  
         
         # save prog and ui
         self.mainProg = mainProg
@@ -25,36 +29,86 @@ class MainWindow (QMainWindow):
         
         # set timer
         self.timer = QTimer()
-        self.timer.setInterval(1000)
+        self.timer.setInterval(500)
         self.timer.timeout.connect(self.modeLED)
         
         # declare IO
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(7,GPIO.OUT)
-        GPIO.setup(11,GPIO.OUT)
-        
+        #GPIO.setmode(GPIO.BOARD)
+        #GPIO.setup(7,GPIO.OUT)
+        #GPIO.setup(11,GPIO.OUT)
+        #GPIO.setup(15,GPIO.OUT)
+ 
     # mode 1
     def mode1Start(self):
         self.mode = 1
-        self.timer.start()
+        self.step = 0
+        self.modeLED()
 
     # mode 2
     def mode2Start(self):
         self.mode = 2
-        self.timer.start()
+        self.step = 0
+        self.modeLED()
         
     # stop
     def modeStop(self):
         self.mode = 0 
+        self.step = 0
         self.timer.stop()
     
     def modeLED(self):
-
+        
+        ## mode 1
         if self.mode == 1:
-            GPIO.output(7,True)
             
+            if (self.step == 0):
+                print('step 0')
+                #GPIO.output(7,True)
+                #GPIO.output(11,False)
+                #GPIO.output(15,False)
+            if (self.step == 1):          
+                print('step 1')
+                #GPIO.output(7,False)
+                #GPIO.output(11,True)
+                #GPIO.output(15,False)
+            if (self.step == 2):
+                print('step 2')
+                #GPIO.output(7,False)
+                #GPIO.output(11,False)
+                #GPIO.output(15,True)
+
+            self.step += 1
+            
+            if (self.step >= 3):
+                self.step = 0
+        
+        ## mode 2
         if self.mode == 2:
-            GPIO.output(7,False)
+            
+            if (self.step == 0):
+                print('step 0')
+                #GPIO.output(7,False)
+                #GPIO.output(11,True)
+                #GPIO.output(15,False)
+            if (self.step == 1):          
+                print('step 1')
+                #GPIO.output(7,True)
+                #GPIO.output(11,False)
+                #GPIO.output(15,True)
+            if (self.step == 2):
+                print('step 2')
+                #GPIO.output(7,False)
+                #GPIO.output(11,False)
+                #GPIO.output(15,False)
+
+            self.step += 1
+            
+            if (self.step >= 3):
+                self.step = 0
+        
+            
+        self.timer.start()
+            
 
         
         
